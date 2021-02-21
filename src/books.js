5936660efb0d293dac85a262f10d078ea7dcd917
sapdeleon <sapdeleon@msn.com>
2021-02-20 22:10:01 -0500

    Initial Upload

---
+++ src/books.js
@@ -0,0 +1,23 @@
+const books = [
+  {
+    id: 1,
+    img: "https://m.media-amazon.com/images/I/51tvbsts5KL._SY346_.jpg",
+    title: "Beyond the Tracks: A WW2 Novel Based on Harrowing True Events",
+    author: "Michael Reit",
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/51fLddHRdEL._SX260_.jpg",
+    title: "Pam Penguin's First Day of School: Will I Fit In?",
+    author: "Ashleigh Benson ",
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/I/51ynG9ODpNL._SY346_.jpg",
+    title:
+      "GIRL JACKED: A Mystery Thriller Novel (Detective Jack Stratton Mystery Thriller Series Book 1)",
+    author: "Christopher Greyson",
+  },
+];
+
+export default books;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import books from "./books";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// function Book({ img, title, author, children }) {
+function Book({ img, title, author }) {
+  return (
+    <article className="book">
+      <img src={img} alt="book-img" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className="booklist">
+      {books.map((book) => (
+        <Book key={book.id} {...book} />
+      ))}
+    </section>
+  );
+}
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
